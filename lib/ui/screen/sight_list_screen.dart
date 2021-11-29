6aacc12f598c94333eb0158473edd96dc3e3614e
lifectrl <mylifectrl@gmail.com>
2021-11-29 10:23:29 +0300

    4.4 Add app bar rich title

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -16,13 +16,28 @@ class _SightListScreenState extends State<SightListScreen> {
         backgroundColor: Colors.transparent,
         elevation: 0,
         centerTitle: false,
-        title: const Text(
-          'Список\nинтересных мест',
-          style: TextStyle(
-            color: Color(0xff252849),
-            fontFamily: 'Roboto',
-            fontSize: 32,
-            fontWeight: FontWeight.w700,
+        title: RichText(
+          textAlign: TextAlign.left,
+          maxLines: 2,
+          text: const TextSpan(
+            style: TextStyle(
+              color: Color(0xff252849),
+              fontFamily: 'Roboto',
+              fontSize: 32,
+              fontWeight: FontWeight.w700,
+            ),
+            children: [
+              TextSpan(
+                text: 'С',
+                style: TextStyle(color: Colors.green),
+              ),
+              TextSpan(text: 'писок\n'),
+              TextSpan(
+                text: 'и',
+                style: TextStyle(color: Colors.yellow),
+              ),
+              TextSpan(text: 'нтересных мест'),
+            ],
           ),
         ),
       ),
